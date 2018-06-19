b6157afac25fd8c5ca388cdb4ca6ede88f5cd8ad
lac-user <Lac_Data_Science@NTUCLINKSG.onmicrosoft.com>
2018-06-19 05:11:59 +0000

    update config

---
+++ docs/_site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,46 @@
+var store = [{
+        "title": "[kaggle] Tree based models in R on Titanic Data",
+        "excerpt":"This is the first time I blog my journey of learning data science, which starts from the first kaggle competition I attempted - the Titanic. In this competition, we are asked to predict the survival of passengers onboard, with some information given, such as age, gender, ticket fare… Translated letter...","categories": ["kaggle"],
+        "tags": ["kaggle,","titanic,","random","forest,","decision","tree"],
+        "url": "http://localhost:4000/DataStory/kaggle/kaggle-titanic-tutorial/",
+        "teaser":null},{
+        "title": "[vis] Spatial Visualization with ggmap R package",
+        "excerpt":"ggmap, which is an R package built to visualize with map, is very similar to ggplot2. And its output is ‘ggplot’ class, which means it also support layered visualization just like ggplot2. I will demonstrate with two examples “Crimes in San Fransisco” and “Taxi in Porto”. The data of both...","categories": ["kaggle,","visualization"],
+        "tags": [],
+        "url": "http://localhost:4000/DataStory/kaggle,/visualization/spatial-viz-ggmap/",
+        "teaser":null},{
+        "title": "[kaggle] Recognize the Digits",
+        "excerpt":"This time I am going to demostrate the kaggle 101 level competition - digit recogniser. We are asked to train a model to recogize the digit from the pixel data in this competition. The data set is available here.description of the data: label: the integers from 0 - 9; features:...","categories": ["kaggle,","SVM,","PCA"],
+        "tags": [],
+        "url": "http://localhost:4000/DataStory/kaggle,/svm,/pca/kaggle-digit-recogition/",
+        "teaser":null},{
+        "title": "[vis] Getting Started With Tableau",
+        "excerpt":"Intro to Tableau Aspired by the course ‘Data Visualization’ offered by University of Illinois on Cousera, I have worked on the interactive data visualization using Tableau. There is a free version of Tableau Public is available and you can upload the visualization online for sharing. Tableau is one of the...","categories": ["visualization"],
+        "tags": [],
+        "url": "http://localhost:4000/DataStory/visualization/get-started-tableau/",
+        "teaser":null},{
+        "title": "[notes] Review on Stanford Machine Learning Course",
+        "excerpt":"I have been signed up for this course for a long time. And since last week, I finally managed to complete it with a good score. This course is taught by Andrew Ng, who is also the co-founder of Cousera. The content of the course spans from supervised learning to...","categories": ["notes"],
+        "tags": [],
+        "url": "http://localhost:4000/DataStory/notes/reivew-course-stanford-ml/",
+        "teaser":null},{
+        "title": "[web-scraping] Job Hunting Like A Data Analyst (Part I)",
+        "excerpt":"Motivation: I’m currently suffering a tough time in looking for a data analyst job.Instead of doing it in a traditional way, I am thinking why not do the job hunting just like a data analyst, by making use of the advantages of data science. Always, the first step to flight...","categories": ["web-scraping"],
+        "tags": [],
+        "url": "http://localhost:4000/DataStory/web-scraping/job-hunting-web-scraping/",
+        "teaser":null},{
+        "title": "[exploratory analysis] Job Hunting Like A Data Analyst (Part II)",
+        "excerpt":"Continued with previous post, I’ve added some additional lines of codes to fetch the job description of each job post. This will take a bit longer time, which is about (1.5 hour) for me, because I set a delay of ~10 seconds between each request. This week I will continue...","categories": ["exploratory","analysis"],
+        "tags": [],
+        "url": "http://localhost:4000/DataStory/exploratory/analysis/job-hunting-exploratory-analysis/",
+        "teaser":null},{
+        "title": "[RecSys] Implementation of Model Based Recommendation System in R",
+        "excerpt":"The most straight forward recommendation system are either user based CF (collaborative filtering) or item based CF, which are categorized as memory based methods. User-Based CF is to recommend products based on behaviour of similar users, and the Item-Based CF is to recommend similar products from products that user purchased....","categories": ["recsys"],
+        "tags": [],
+        "url": "http://localhost:4000/DataStory/recsys/model-based-recsys-r/",
+        "teaser":null},{
+        "title": "[DL] Digit Recognition with Tensor Flow",
+        "excerpt":"This time I am going to continue with the kaggle 101 level competition – digit recogniser with deep learning tool Tensor Flow. In the previous post, I used PCA and Pooling methods to reduce the dimensions of the dataset, and train with the linear SVM. Due to the limited efficiency...","categories": ["deep","learning"],
+        "tags": [],
+        "url": "http://localhost:4000/DataStory/deep/learning/digit-recognition-tensorflow/",
+        "teaser":null},]
